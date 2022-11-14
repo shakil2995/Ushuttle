c28534d17de9dfe93a25eeea755647197b56b9cc
Shakil <shakil2995@gmail.com>
2022-11-15 01:05:26 +0600

    live map

--- lib/live_map_page.dart
+++ lib/live_map_page.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:latlong2/latlong.dart';
 
+var lat = 23.874370;
+var lng = 90.390766;
 void main() {
   runApp(MaterialApp(
     home: LiveMapPage(),
@@ -16,50 +19,100 @@ class LiveMapPage extends StatefulWidget {
 class _LiveMapPageState extends State<LiveMapPage> {
   @override
   Widget build(BuildContext context) {
-    var marker = <Marker>[];
+// markers
 
+//  load current location permission
+    getCurrentLocation() {
+      Future<Position> getUserCurrentLocation() async {
+        await Geolocator.requestPermission()
+            .then((value) {})
+            .onError((error, stackTrace) {});
+        return await Geolocator.getCurrentPosition();
+      }
+
+//  load user current location
+      getUserCurrentLocation().then((value) {
+        lat = value.latitude;
+        lng = value.longitude;
+        print('My current location');
+        print(value.latitude.toString() + " " + value.longitude.toString());
+      });
+    }
+
+    @override
+    void initState() {
+      super.initState();
+      getCurrentLocation();
+    }
+
+    var marker = <Marker>[];
     marker = [
-      Marker(
-        point: LatLng(23.874370, 90.390766),
-        builder: (ctx) => const Icon(
-          Icons.circle_sharp,
-          color: Colors.blue,
-          // size: 28,
-        ),
-      ),
-      Marker(
-        point: LatLng(23.875246, 90.389599),
-        builder: (ctx) => Icon(Icons.bus_alert),
-      ),
+      // Marker(
+      //   point: LatLng(23.874370, 90.390766),
+      //   builder: (ctx) => const Icon(
+      //     Icons.circle_sharp,
+      //     color: Colors.blue,
+      //     // size: 28,
+      //   ),
+      // ),
+      // Marker(
+      //   point: LatLng(23.875246, 90.389599),
+      //   builder: (ctx) => Icon(Icons.bus_alert),
+      // ),
       // Marker(
       //   point: LatLng(23.864370, 90.390766),
       //   builder: (ctx) => Icon(Icons.circle),
       // ),
     ];
+    // loadData() {
+    //     setState(() {
+    //       marker.add(
+    //         Marker(
+    //           point: LatLng(lat, lng),
+    //           builder: (ctx) => Icon(
+    //             Icons.circle_sharp,
+    //             color: Colors.blue,
+    //           ),
+    //         ),
+    //       );
+    //     });
+    //   }
 
     return Scaffold(
-      body: Center(
-          child: Container(
-        child: Column(
-          children: [
-            Flexible(
-              child: FlutterMap(
-                options: MapOptions(
-                  center: LatLng(23.874370, 90.390766),
-                  zoom: 16,
+// add floating action button
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          getCurrentLocation();
+          setState(() {
+            body:
+            Center(
+              child: Container(
+                child: Column(
+                  children: [
+                    Flexible(
+                      child: FlutterMap(
+                        options: MapOptions(
+                          center: LatLng(lat, lng),
+                          zoom: 16,
+                        ),
+                        layers: [
+                          TileLayerOptions(
+                            urlTemplate:
+                                'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+                          ),
+                          MarkerLayerOptions(markers: marker),
+                        ],
+                      ),
+                    )
+                  ],
                 ),
-                layers: [
-                  TileLayerOptions(
-                    urlTemplate:
-                        'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-                  ),
-                  MarkerLayerOptions(markers: marker)
-                ],
               ),
-            ),
-          ],
-        ),
-      )),
+            );
+          });
+        },
+        child: const Icon(Icons.my_location),
+        // backgroundColor: Colors.blue,
+      ),
     );
   }
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,9 +6,11 @@ import FlutterMacOS
 import Foundation
 
 import firebase_core
+import geolocator_apple
 import location
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   LocationPlugin.register(with: registry.registrar(forPlugin: "LocationPlugin"))
 }
