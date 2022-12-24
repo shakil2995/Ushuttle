85e616d12d2a759c084f7a52bea4e0da384c004f
Shakil <shakil2995@gmail.com>
2022-12-25 03:48:15 +0600

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -17,22 +17,22 @@ Future<void> main() async {
   runApp(const MyApp());
 }
 
-const MaterialColor customBlack = MaterialColor(
-  _customBlackPrimaryValue,
-  <int, Color>{
-    50: Color(0xFFECEFF1),
-    100: Color(0xFFCFD8DC),
-    200: Color(0xFFB0BEC5),
-    300: Color(0xFF90A4AE),
-    400: Color(0xFF78909C),
-    500: Color(_customBlackPrimaryValue),
-    600: Color(0xFF546E7A),
-    700: Color(0xFF455A64),
-    800: Color(0xFF37474F),
-    900: Color(0xFF263238),
-  },
-);
-const int _customBlackPrimaryValue = 0xFF263238;
+// const MaterialColor customBlack = MaterialColor(
+//   _customBlackPrimaryValue,
+//   <int, Color>{
+//     50: Color(0xFFECEFF1),
+//     100: Color(0xFFCFD8DC),
+//     200: Color(0xFFB0BEC5),
+//     300: Color(0xFF90A4AE),
+//     400: Color(0xFF78909C),
+//     500: Color(_customBlackPrimaryValue),
+//     600: Color(0xFF546E7A),
+//     700: Color(0xFF455A64),
+//     800: Color(0xFF37474F),
+//     900: Color(0xFF263238),
+//   },
+// );
+// const int _customBlackPrimaryValue = 0xFF263238;
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
