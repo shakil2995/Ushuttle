6dab669f81f65f709422cccbc5d601968977f18a
Shakil <shakil2995@gmail.com>
2022-12-25 17:22:01 +0600

    added splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:ushuttlev1/widget_tree.dart';
 // import 'package:ushuttlev1/subPages/live_map_page.dart';
 import 'firebase_options.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 // import 'live_location_page.dart';
 
 Future<void> main() async {
